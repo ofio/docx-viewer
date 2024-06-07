0d41ea2d3b93778d66d4de1204cf2370bbc37eb7
黄小米 <nn880328@126.com>
2024-06-07 15:58:07 +0800

    style:rewrite docx-preview.d.ts

--- dist/docx-preview.d.ts
+++ dist/docx-preview.d.ts
@@ -1,20 +1,27 @@
 export interface Options {
-    inWrapper: boolean;
-    ignoreWidth: boolean;
-    ignoreHeight: boolean;
-    ignoreFonts: boolean;
-    breakPages: boolean;
-    debug: boolean;
-    experimental: boolean;
-    className: string;
-    trimXmlDeclaration: boolean;
-    renderHeaders: boolean;
-    renderFooters: boolean;
-    renderFootnotes: boolean;
-    renderEndnotes: boolean;
-    ignoreLastRenderedPageBreak: boolean;
-    useBase64URL: boolean;
-    renderChanges: boolean;
+	breakPages: boolean;                    //enables page breaking on page breaks
+	className: string;                      //class name/prefix for default and document style classes
+
+	ignoreFonts: boolean;                   //disables fonts rendering
+	ignoreHeight: boolean;                  //disables rendering height of page
+	ignoreImageWrap: boolean;               //disables image text wrap setting
+	ignoreLastRenderedPageBreak: boolean;   //disables page breaking on lastRenderedPageBreak elements
+	ignoreTableWrap: boolean;               //disables table's text wrap setting
+	ignoreWidth: boolean;                   //disables rendering width of page
+
+	inWrapper: boolean;                     //enables rendering of wrapper around document content
+
+	renderChanges: boolean;                 //enables experimental rendering of document changes (inserions/deletions)
+	renderEndnotes: boolean;                //enables endnotes rendering
+	renderFooters: boolean;                 //enables footers rendering
+	renderFootnotes: boolean;               //enables footnotes rendering
+	renderHeaders: boolean;                 //enables headers rendering
+
+	trimXmlDeclaration: boolean;            //if true, xml declaration will be removed from xml documents before parsing
+	useBase64URL: boolean;                  //if true, images, fonts, etc. will be converted to base 64 URL, otherwise URL.createObjectURL is used
+
+	debug: boolean;                         //enables additional logging
+	experimental: boolean;                  //enables experimental features (tab stops calculation)
 }
 
 export declare const defaultOptions: Options;
